6790459e0a34408be95af5a177cdc35a2763f1d7
Chaliuk Vladyslav <chaliukvladyslav@gmail.com>
2024-03-25 16:08:27 +0200

    add deploy

---
+++ tspp/settings-prod.py
@@ -0,0 +1,17 @@
+from .settings import *
+import os
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
