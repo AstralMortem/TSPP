89b8ee644ece1b23b7df4547f5b68dfe6425767f
Vladyslav Chaliuk <chaliukvladyslav@gmail.com>
2024-03-26 21:20:43 +0200

    remove chanels layer

--- tspp/settings.py
+++ tspp/settings.py
@@ -80,12 +80,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "tspp.wsgi.application"
 ASGI_APPLICATION = "tspp.asgi.application"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
-}
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
